6c3009ce93ad318dcd8e6fa2a5e4443bdf56a73b
MAmph-gif <mario.anfora@gmail.com>
2026-09-14 14:22:50 +0200

    Delete js directory

--- js/verbali/invito650/invito650.ui.js
+++
@@ -1,7 +0,0 @@
-/* ==========================================================================
-   MODULE: INVITO DI PRESENTAZIONE (ART. 650 C.P.) - UI
-   ========================================================================== */
-
-export function syncInvito650UI(){
-  /* nessuna visibilità condizionale in questa pratica */
-}
